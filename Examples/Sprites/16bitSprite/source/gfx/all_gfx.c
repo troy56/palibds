a73d3fed719d12486a6ec93e3720acb8a6796b74
molousk <molousk@70d487c4-6a1c-0410-9ca0-4d1b8e83941e>
2006-09-12 12:16:56 +0000

    Examples uploaded

---
+++ Examples/Sprites/16bitSprite/source/gfx/all_gfx.c
@@ -0,0 +1,9 @@
+//Gfx converted using Mollusk's PAGfxConverter
+
+//This file contains all the .c, for easier inclusion in a project
+
+
+// Sprite files : 
+#include "ship.c"
+
+// Palette files : 
---
+++ Examples/Sprites/SpriteAnim3/source/gfx/all_gfx.h
@@ -0,0 +1,15 @@
+//Gfx converted using Mollusk's PAGfxConverter
+
+//This file contains all the .h, for easier inclusion in a project
+#ifndef __ALL_GFX__
+#define __ALL_GFX__
+
+
+// Sprite files : 
+extern const unsigned char explosion_Sprite[28672];  // Pal : explosion_Pal
+
+// Palette files : 
+extern const unsigned short explosion_Pal[15];
+
+#endif
+
